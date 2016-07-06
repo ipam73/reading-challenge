4ceaa6a0fb4436159e256d77dfc37c0143d1460a
Pam <pamela.martinez@clever.com>
2016-07-06 00:24:10 -0700

    add support for native linking

--- app/native/components/Support.js
+++ app/native/components/Support.js
@@ -5,7 +5,9 @@ import {
   TouchableHighlight,
   Text,
   Image,
+  Linking,
 } from 'react-native';
+import Button from 'apsl-react-native-button';
 
 const icon = require('../../images/baltimore_city_logo.jpg');
 
@@ -53,26 +55,64 @@ var styles = StyleSheet.create({
     flex: 1,
     alignSelf: 'center',
   },
+  buttonLink: {
+    marginTop: 15,
+    borderColor: 'gray',
+    backgroundColor: 'white',
+    borderRadius: 0,
+    borderWidth: 2,
+    width: 200,
+    height: 30,
+    alignSelf: 'center',
+  },
+  buttonLinkText: {
+    fontSize: 14,
+  },
+
 });
 
-function Support() {
-  return (
-    <View style={styles.main}>
-      <TouchableHighlight style={styles.row}>
-        <View style={styles.headingContainer}>
-          <View style={styles.headingText}>
-            <Text style={styles.headingTitle}>Contact Information</Text>
-            <Image style={styles.icon} source={icon} />
-            <Text style={styles.normalText}>
-              If you have any questions or concerns about the reading challenge,
-              please see your child’s school for more information. If you have technical
-               issues with the use of this app, please email CitySchoolsIT@bcps.k12.md.us.
-            </Text>
+class Support extends React.Component {
+
+  linkToSupport() {
+    const url = 'https://drive.google.com/file/d/0B9SRyd85Yz8ZQ09sQktjeTY2czQ/view?usp=drivesdk';
+    Linking.canOpenURL(url).then(supported => {
+      if (!supported) {
+        console.log('Can\'t handle url: ' + url);
+      } else {
+        return Linking.openURL(url);
+      }
+    }).catch(err => console.error('An error occurred', err));
+  }
+
+  render() {
+    return (
+      <View style={styles.main}>
+        <TouchableHighlight style={styles.row}>
+          <View style={styles.headingContainer}>
+            <View style={styles.headingText}>
+              <Text style={styles.headingTitle}>Contact Information</Text>
+              <Image style={styles.icon} source={icon} />
+              <Text style={styles.normalText}>
+                If you have any questions or concerns about the reading challenge,
+                please see your child’s school for more information. If you have technical
+                 issues with the use of this app, please email CitySchoolsIT@bcps.k12.md.us.
+              </Text>
+            </View>
           </View>
-        </View>
-      </TouchableHighlight>
-    </View>
-  );
+        </TouchableHighlight>
+        <TouchableHighlight style={styles.row}>
+          <View style={styles.headingContainer}>
+            <View style={styles.headingText}>
+              <Text style={styles.headingTitle}>Getting Started</Text>
+              <Button style={styles.buttonLink} textStyle={styles.buttonLinkText} onPress={this.linkToSupport.bind(this)}>
+                See Guide
+              </Button>
+            </View>
+          </View>
+        </TouchableHighlight>
+      </View>
+    );
+  }
 }
 
 module.exports = Support;
