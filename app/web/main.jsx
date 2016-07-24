abf814374303e38d1d1476ec879c06a169356c83
Mohit Gupta <mohit.ed@ischool.berkeley.edu>
2016-07-23 19:31:42 -0700

    basic google analytics

--- app/web/main.jsx
+++ app/web/main.jsx
@@ -20,10 +20,12 @@ import actions from '../actions';
 var reducer = combineReducers({reducers, routerReducer});
 
 // log page loads to google analytics
-ReactGA.initialize('UA-81211784-1', {debug: true});
+ReactGA.initialize('UA-81211784-1');
 function logGoogleAnalytics() {
-  ReactGA.set({page: window.location.pathname});
-  ReactGA.pageview(window.location.pathname);
+  // example: http://localhost:5000/#/login?_k=7w950a sets path to #/login
+  var reactPath = window.location.pathname + window.location.hash.split("?")[0]
+  ReactGA.set({page: reactPath});
+  ReactGA.pageview(reactPath);
 }
 
 // store, takes reducer, thunk middleware
